ff07b3a9764af9a89eb2d154985ccc4a44f9d226
beardsleym <matthew@beardsley.com.au>
2022-12-13 21:07:30 -0500

    remove mail icon

--- assets/Icons.tsx
+++ assets/Icons.tsx
@@ -2,14 +2,6 @@ import { AudibleIcon } from "./Audible";
 import { IMDBIcon } from "./IMDB";
 import { TwitterIcon } from "../assets/Twitter";
 import { InstagramIcon } from "../assets/Instagram";
-import { MailIcon } from "../assets/Mail";
 import { SpotlightIcon } from "../assets/Spotlight";
 
-export {
-  AudibleIcon,
-  IMDBIcon,
-  TwitterIcon,
-  InstagramIcon,
-  MailIcon,
-  SpotlightIcon,
-};
+export { AudibleIcon, IMDBIcon, TwitterIcon, InstagramIcon, SpotlightIcon };
